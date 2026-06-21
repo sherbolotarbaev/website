2485337d96305ef7f2875e6553b6450827a4fa5a
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2026-06-21 09:11:35 +0600

    add: shader bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Shader from '@/components/ui/shader'
 import { HeroSection } from 'sections/hero'
 
 export default function Home() {
 	return (
 		<div className='dark:bg-[#151515] flex min-h-screen items-center justify-center bg-background'>
-			<main className='flex min-h-screen w-full flex-col items-center px-4 py-12 justify-center'>
+			<main className='relative flex min-h-screen w-full flex-col items-center px-2 py-8 justify-center'>
+				<Shader />
 				<HeroSection />
 			</main>
 		</div>
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -239,7 +239,7 @@ const educations: EducationItemType[] = [
 
 export function HeroSection() {
 	return (
-		<div className='w-full max-w-xl flex items-center justify-center'>
+		<div className='relative w-full max-w-xl flex items-center justify-center rounded-4xl backdrop-blur-sm bg-background/80 p-4'>
 			<div className='w-full h-full flex flex-col gap-8'>
 				<SectionBadge
 					className='w-fit'
